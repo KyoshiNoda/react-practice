a34a0ca2de3b2897850f739ec96b142e134f59ac
unknown <kyoshisew@gmail.com>
2022-10-09 23:14:00 -0500

    added tailwind

---
+++ tailwind-test/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind-test/src/components/Form.js
@@ -0,0 +1,11 @@
+const Form = props =>{
+    return(
+        <div>
+        <h1 className="text-9xl font-bold underline ">
+        Hello world!
+        </h1>
+        </div>
+    )
+}
+
+export default Form;
