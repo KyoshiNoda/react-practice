3b5f60fc30b0e221d8bab019d6d13d56b76afbee
unknown <kyoshisew@gmail.com>
2022-10-14 14:39:51 -0500

    added form

--- tailwind-test/src/components/Form.jsx
+++ tailwind-test/src/components/Form.jsx
@@ -1,27 +1,36 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Container from './Container';
 const Form = () => {
 
-    const [item,setItem] = useState();
+    const [item, setItem] = useState();
 
-    const itemHandler = (event) =>{
+    const formHandler = (event) =>{
+        event.preventDefault();
+        if (item.trim() === 0){
+            return;
+        }
+    }
+
+    const itemHandler = (event) => {
         setItem(event.target.value)
         console.log(item);
     };
-    
+
     return (
         <Container>
             <div className='container'>
-                    <label className = "block text-white text-center text-2xl" htmlFor='input'>To do</label>
+                <form onSubmit={formHandler}>
+                    <label className="block text-white text-center text-2xl" htmlFor='input'>To do</label>
 
                     <div className='flex justify-center'>
-                        <input className = "" 
-                        type = 'text' 
-                        id = 'input'
-                        value = {item}
-                        onChange = {itemHandler}
+                        <input className=""
+                            type='text'
+                            id='input'
+                            value={item}
+                            onChange={itemHandler}
                         />
                     </div>
+                </form>
             </div>
         </Container>
     );
