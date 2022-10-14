ff3b7bea723b625b9f8b367f5ce666916d47f20c
unknown <kyoshisew@gmail.com>
2022-10-14 14:35:37 -0500

    added working list

--- tailwind-test/src/components/Form.jsx
+++ tailwind-test/src/components/Form.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Container from './Container';
 const Form = () => {
+
+    const [item,setItem] = useState();
+
+    const itemHandler = (event) =>{
+        setItem(event.target.value)
+        console.log(item);
+    };
+    
     return (
         <Container>
             <div className='container'>
                     <label className = "block text-white text-center text-2xl" htmlFor='input'>To do</label>
 
                     <div className='flex justify-center'>
-                        <input classname = "" type = 'text' id = 'input'/>
+                        <input className = "" 
+                        type = 'text' 
+                        id = 'input'
+                        value = {item}
+                        onChange = {itemHandler}
+                        />
                     </div>
             </div>
         </Container>
