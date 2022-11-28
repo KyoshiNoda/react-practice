05ec8492c42c8329b0dc42015fb413f4a9f991de
KyoshiNoda <kyoshisew@gmail.com>
2022-11-28 15:00:54 -0500

    added db query

--- crud-app/backend/index.js
+++ crud-app/backend/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'crud-app'
+});
+
+app.get("/", (req,res)=>{
+  const command1 = "INSERT INTO movie_reviews (name,review) VALUES ('black panther','decent movie... could work on plot a little better');";
+  db.query(command1,(err,result) =>{
+    res.send("movie added!");
+  });
+  res.send("nodemon is running");
+});
 
 app.listen(3001,(req,res)=>{
   console.log("listening!");
 });
-app.get("/", (req,res)=>{
-  res.send("nodemon is running");
-})
-
