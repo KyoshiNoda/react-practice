2e7699d25ca34b6a285fdfe00f86c73ee2f8bc47
KyoshiNoda <kyoshisew@gmail.com>
2022-11-28 13:20:15 -0500

    added react router

--- crud-app/client/src/App.js
+++ crud-app/client/src/App.js
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Test from "./pages/Test";
 function App() {
   return (
-    <div className = 'bg-cyan-600'>
-      <p>Hello World</p>
-    </div>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/ViewGroup" element={<Test />} />
+    </Routes>
   );
 }
 
