d9d2df97c1fb6b17664db1f1bf3cb287bd0bcd8a
unknown <kyoshisew@gmail.com>
2023-01-12 01:33:16 -0500

    added db connection

--- account-system/backend/server.js
+++ account-system/backend/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require('mysql');
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'password',
+    database : 'account-system'
+});
 
 app.listen(3001,()=>{
     console.log('listening on port 3001');
@@ -8,7 +21,16 @@ app.listen(3001,()=>{
 app.get('/',(req,res) =>{
     res.send("hello this is working with nodemon");
 });
-
+app.get('/api/db',(req,res) =>{
+    const sqlInsert = "SELECT * FROM `account-system`.accounts;"
+    db.query(sqlInsert,(err,result) =>{
+        res.send(result);
+    })
+})
 app.get('/api/:name',(req,res) =>{
     res.send(req.params.name);
+});
+
+app.post('/api/newUser',(req,res) =>{
+    console.log(req);
 });
