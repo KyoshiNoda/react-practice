d1f6645bdabd63a428a0a4af1aa33c24b8e4bd34
unknown <kyoshisew@gmail.com>
2023-03-15 15:52:40 -0400

    set up project with tailwind

---
+++ redux-practice/client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div className='flex bg-slate-400 justify-center'>
+      hi
+    </div>
+  )
+}
+
+export default App
