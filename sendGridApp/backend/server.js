3b228ccd0b1dbc355f929f4c076e572f4140d2fb
kyoshinoda <kyoshisew@gmail.com>
2023-05-13 22:26:13 -0400

    added test email

--- sendGridApp/backend/server.js
+++ sendGridApp/backend/server.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
 });
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: "test@example.com", // Change to your recipient
-  from: "test@example.com", // Change to your verified sender
+  to: process.env.RECIPIENT,
+  from: process.env.SENDER,
   subject: "Sending with SendGrid is Fun",
   text: "and easy to do anywhere, even with Node.js",
   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
@@ -24,5 +24,5 @@ sgMail
     console.error(error);
   });
 app.listen(3005, () => {
-  console.log("listening on port 3005");
+  console.log("listening on port 3001");
 });
